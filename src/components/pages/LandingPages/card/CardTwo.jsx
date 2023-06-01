45e0306bfa5cdac830ddf83357592c1ce6e73e7d
dnil2202 <dnilsibarani@gmail.com>
2023-06-01 14:05:05 +0700

    update

--- src/components/pages/LandingPages/card/CardTwo.jsx
+++ src/components/pages/LandingPages/card/CardTwo.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Fade from 'react-reveal/Fade';
 
 const CardTwo = () => {
   return (
@@ -9,18 +8,18 @@ const CardTwo = () => {
        </div>
       <div className='md:grid grid-cols-2 gap-5'>
           <div className='my-10'>
-        <Fade left>
+        <>
             <h6 className='text-center md:text-end font-bold'>Misa Harian, Jumat Pertama</h6>
             <h6 className='text-center md:text-end'>Misa Harian : Senin-Sabtu : 05.45</h6>
             <h6 className='text-center md:text-end'>Jumat Pertama : Jumat : 17:00</h6>
-        </Fade>
+        </>
           </div>
         <div className='my-10'>
-          <Fade right>
+          <>
             <h6 className='text-center md:text-start font-bold'>Misa Hari Sabtu dan Minggu</h6>
             <h6 className='text-center md:text-start'>Sabtu : 17:00</h6>
             <h6 className='text-center md:text-start'>Minggu : 06:00, 09:30, 17:00</h6>
-          </Fade>
+          </>
         </div>
       </div>
     </div>
