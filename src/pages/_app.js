0716374b56b94a623112bc93c6e3ad255d638055
dnil2202 <dnilsibarani@gmail.com>
2023-06-01 13:55:24 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,19 @@
 import Layouts from '@/components/layouts/Layouts'
 import '@/styles/globals.css'
+import Head from 'next/head'
+import logo from '../../public/images-removebg-preview.png'
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>St. Ignatius</title>
+      <meta name='description' content='go to church' />
+      <link rel='icon' href={logo} />
+    </Head>
     <Layouts>
       <Component {...pageProps} />
     </Layouts>
+    </>
   )
 }
